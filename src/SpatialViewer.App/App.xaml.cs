ed50c07d82990c1c38a0d74934022f34abd0ab48
Kiyō Jo <x2425618950@gmail.com>
2026-08-30 23:13:44 +0800

    feat: apply saved language before WinUI resource initialization

--- src/SpatialViewer.App/App.xaml.cs
+++ src/SpatialViewer.App/App.xaml.cs
@@ -5,7 +5,13 @@ namespace SpatialViewer.Product;
 public partial class App : Application
 {
     private Window? _window;
-    public App() => InitializeComponent();
+
+    public App()
+    {
+        AppSettingsStore.ApplySavedLanguage();
+        InitializeComponent();
+    }
+
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         _window = new MainWindow();
